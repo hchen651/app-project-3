0094a60b6827c58a5920ad75c4baad69dcaaea99
hchen651 <hchen651@gmail.com>
2019-06-15 13:27:23 -0400

    pages setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Detail from "./pages/Detail";
+import Collection from "./pages/Collection";
+import NewCard from "./pages/NewCard";
+
+// React Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+
+//CSS
+import "./App.css";
 
 function App() {
+  document.title = "Rolodex (Name Pending)";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/detail" component={Detail} />
+          <Route exact path="/collection" component={Collection} />
+          <Route exact path="/newcard" component={NewCard} />
+        </Wrapper>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
