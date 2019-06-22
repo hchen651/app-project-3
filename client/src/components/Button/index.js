3dc092dfce9de47f3f23c4d34a6d05cfb813efc1
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2019-06-22 10:43:37 -0400

    card detail, material ui test

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,10 +1,10 @@
+// material-ui
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-// We can inject some CSS into the DOM.
 const styles = {
   root: {
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
