98694449c77cd172520c1a12b55c2f6078bcb3ac
Henry <hchen651@gmail.com>
2019-06-25 11:16:46 -0400

    fix

--- client/src/pages/Signin/index.js
+++ client/src/pages/Signin/index.js
@@ -11,92 +11,111 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
-    paper: {
-        marginTop: theme.spacing(6),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    form: {
-        width: '100%',
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-}));
 
-// Declare State
-// https://reactjs.org/docs/forms.html
+class SignIn extends React.Component {
 
-// Handle Submit
-const handleSubmit = e => {
-    e.preventDefault();
-};
+    // Declare State
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+            password: "",
+            errors: {}
+        };
+    }
 
-export default function SignIn() {
-    const classes = useStyles();
+    useStyles = makeStyles(theme => ({
+        paper: {
+            marginTop: theme.spacing(6),
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+        form: {
+            width: '100%',
+            marginTop: theme.spacing(1),
+        },
+        submit: {
+            margin: theme.spacing(3, 0, 2),
+        },
+    }));
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <div className={classes.paper}>
-                <Typography component="h1" variant="h5" gutterBottom>
-                Sign In
-                </Typography>
-                <form 
-                className={classes.form} 
-                onSubmit={handleSubmit} 
-                noValidate>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
+    classes = useStyles();
+
+
+    // Handle Submit
+    handleSubmit = e => {
+        e.preventDefault();
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        };
+        console.log(userData);
+    };
+
+    render() {
+        const { errors } = this.state;
+        return (
+            <Container component="main" maxWidth="xs">
+                <div className={classes.paper}>
+                    <Typography component="h1" variant="h5" gutterBottom>
                         Sign In
+                </Typography>
+                    <form
+                        className={classes.form}
+                        onSubmit={handleSubmit}
+                        noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
                     </Button>
-                    
-                    <Grid container justify="center">
-                        <Grid item>
-                        <Link href="/signup" variant="body2">
-                            {"Don't have an account? Sign Up"}
-                        </Link>
+
+                        <Grid container justify="center">
+                            <Grid item>
+                                <Link href="/signup" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </form>
-            </div>
-        </Container>
-    );
+                    </form>
+                </div>
+            </Container>
+        );
+    };
 }
 
+export default SignIn;
 // import React, { useState } from "react";
 
 // // React Components
