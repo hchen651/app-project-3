ee740ecb03c96195e7abe8e1d1e205a0021441f8
Henry <hchen651@gmail.com>
2019-06-28 22:27:24 -0400

    added camera for testing

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -25,6 +25,20 @@ const userSeed = [
   },
   {
     password: "testpass",
+    firstName: "Rachael",
+    lastName: "Tseng",
+    cardType: "Personal",
+    companyName: "imprint Inc.",
+    email: "project@imprint.inc",
+    phone: "(718)123-4567",
+    website: "project-imprint.herokuapp.com",
+    street: "123 Fake St.",
+    city: "New York",
+    state: "NY",
+    zipcode: "10003"
+  },
+  {
+    password: "testpass",
     firstName: "Henry",
     lastName: "Chen",
     cardType: "Personal",
@@ -107,17 +121,17 @@ const cardSeed = [
   }
 ];
 
-db.User
-  .remove({})
-  .then(() => db.User.collection.insertMany(userSeed))
-  .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+// db.User
+//   .remove({})
+//   .then(() => db.User.collection.insertMany(userSeed))
+//   .then(data => {
+//     console.log(data.result.n + " records inserted!");
+//     process.exit(0);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//     process.exit(1);
+//   });
 
 db.Card
   .remove({})
